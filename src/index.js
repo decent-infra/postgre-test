e73a4db7649f48dc653da75aefd34be7b3185e78
shomix <shomixpetrovic@gmail.com>
2023-06-16 12:57:45 +0200

    Logs

--- src/index.js
+++ src/index.js
@@ -5,20 +5,23 @@ for (let i = 0; i < 10; i++) {
   console.log("Hello, world!");
 }
 
-// Configure the PostgreSQL connection
-const pool = new Pool({
-  user: "admin",
-  host: "praetor.ingress.dcnorse.ddns.net",
-  database: "mydb",
-  password: "password",
-  port: 32490, // Default PostgreSQL port
-});
-
 // Create an HTTP server
 const server = http.createServer(async (req, res) => {
   try {
+    // Configure the PostgreSQL connection
+    console.log("Connecting to PostgreSQL database...");
+    const pool = new Pool({
+      user: "admin",
+      host: "praetor.ingress.dcnorse.ddns.net",
+      database: "mydb",
+      password: "password",
+      port: 32490, // Default PostgreSQL port
+    });
     const client = await pool.connect();
+    console.log("Connected to PostgreSQL database");
+    console.log("Sending query to PostgreSQL database");
     const result = await client.query('SELECT * FROM "Books"');
+    console.log("Received response from PostgreSQL database");
     const books = result.rows;
 
     res.statusCode = 200;
